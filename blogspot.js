f0ba67b51476b12286d524518f036a620f3628aa
ajax-google <44095135+ajax-google@users.noreply.github.com>
2018-10-13 06:49:28 +0700

    Update blogspot.js

--- blogspot.js
+++ blogspot.js
@@ -7,3 +7,4 @@ var ex2 = dot[3];
 if (ex != &quot;com&quot; || ex2 != null){
 var name = dot[0] + &quot;.blogspot.com/ncr&quot;;
 window.location.replace(&quot;https://&quot; + name + path);}}
+if (window.jstiming) window.jstiming.load.tick(&#39;headEnd&#39;);
