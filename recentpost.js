62484c9356d5f79c3e49273a1426b8bf8a0da2f3
ajax-google <44095135+ajax-google@users.noreply.github.com>
2018-10-14 10:30:03 +0700

    Delete recentpost.js

--- recentpost.js
+++
@@ -1,3 +0,0 @@
-<script type="text/javascript">
-function showrecentposts(json){for(var i=0;i<numposts;i++){var entry=json.feed.entry[i];var posttitle=entry.title.$t;var posturl;if(i==json.feed.entry.length)break;for(var k=0;k<entry.link.length;k++){if(entry.link[k].rel=='alternate'){posturl=entry.link[k].href;break}}posttitle=posttitle.link(posturl);if(standardstyling)document.write('<li>');document.write(posttitle)}if(standardstyling)document.write('</li>')} </script><ul>
-<script type="text/javascript">var numposts = 5;var standardstyling = true;</script><script type="text/javascript" src="/feeds/posts/default?orderby=published&amp;alt=json-in-script&amp;callback=showrecentposts"></script></ul>
